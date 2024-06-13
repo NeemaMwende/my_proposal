faa1a7e5b3fd6344b2a7a8b2c47b843373f415de
NeemaMwende <gracepete12@gmail.com>
2024-06-13 20:51:25 +0300

    updates

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Initial from './Initial';
+
+function App() {
+  return (
+    <div className="App">
+      <Initial />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Initial.jsx
@@ -0,0 +1,35 @@
+// src/Valentine.js
+import React, { useState } from "react";
+import "./App.css";
+
+const Initial = () => {
+  const [fontSize, setFontSize] = useState(16); // initial font size
+
+  const handleNoButtonClick = () => {
+    setFontSize((prevFontSize) => prevFontSize + 10);
+  };
+
+  return (
+    <div>
+      <div id="valentineQuestion"><b>Will you be my valentine?</b></div>
+      <button
+        className="answerButton"
+        onClick={() => window.location.href = 'thankyou.html'}
+        style={{ fontSize: `${fontSize}px` }}
+      >
+        Yes
+      </button>
+      <button
+        className="answerButton"
+        id="noButton"
+        onClick={handleNoButtonClick}
+      >
+        I no no wanna o(╥﹏╥)o
+      </button>
+      <br />
+      <img src="/cat copy.gif" alt="cat asking question" className="responsive" />
+    </div>
+  );
+};
+
+export default Initial;
